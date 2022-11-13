f29d52105fda476a55e77436b4918b5f82998ac3
mixnuts211 <migyeong0211@gmail.com>
2022-11-13 22:54:31 +0900

    color-fliper js dom select

---
+++ src/js/color-flipper.js
@@ -0,0 +1,3 @@
+const colorArr = [a, b, c, d, e, f, 0, 1, 2, 3, 4, 5, 6, 7,8, 9, 10];
+const mainSection = document.querySelector(".mainSection");
+const switchBtn = mainSection.querySelector(".switch-bth");
