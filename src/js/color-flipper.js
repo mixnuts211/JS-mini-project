165e852e3625b50fcb9ea6a3e62ce2a8b01e96d1
mixnuts211 <migyeong0211@gmail.com>
2022-11-14 00:06:39 +0900

    color-flipper 간단한 코드 style 수정

--- src/js/color-flipper.js
+++ src/js/color-flipper.js
@@ -1,3 +1,4 @@
-const colorArr = [a, b, c, d, e, f, 0, 1, 2, 3, 4, 5, 6, 7,8, 9, 10];
-const mainSection = document.querySelector(".mainSection");
+const colorArr = ["a", "b", "c", "d", "e", "f", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const mainSection = document.querySelector(".full-screen-section");
 const switchBtn = mainSection.querySelector(".switch-bth");
